cae4e54d5a40180554eda3530d68a96ed403d4bc
ErHamza <heerraji123@gmail.com>
2023-05-18 13:03:40 +0100

    p

--- src/app/admin/doctors-management/doctors-management.component.ts
+++ src/app/admin/doctors-management/doctors-management.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-doctors-management',
   templateUrl: './doctors-management.component.html',
   styleUrls: ['./doctors-management.component.css']
 })
-export class DoctorsManagementComponent implements OnInit {
-
-  constructor() { }
+export class DoctorsManagementComponent implements OnInit , DoCheck {
+segments? : string[];
+  constructor(private router : Router) { }
+  getCurrentUrl(){
+    this.segments = this.router.url.split("/").filter(segment=> segment != '')
+  }
+  ngDoCheck() {
+    this.getCurrentUrl()
+  }
 
   ngOnInit(): void {
+    this.getCurrentUrl()
+    
+    
   }
 
 }
--- src/app/admin/patients/patients.component.ts
+++ src/app/admin/patients/patients.component.ts
@@ -1,18 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 import { PatientService } from 'src/app/services/patient.service';
 import { Patient } from 'src/app/models/patient.model';
 import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-patients',
   templateUrl: './patients.component.html',
   styleUrls: ['./patients.component.css']
 })
-export class PatientsComponent implements OnInit {
+export class PatientsComponent implements OnInit , DoCheck {
 
-  constructor(private patientService : PatientService) { }
+  constructor(private patientService : PatientService , private router : Router) { }
+  ngDoCheck() {
+    this.getCurrentUrl()
+    
+  }
+  segments? : string[];
+  
+  getCurrentUrl(){
+    this.segments = this.router.url.split("/").filter(segment=> segment != '')
+  }
 
   ngOnInit(): void {
+    this.getCurrentUrl()
     
   }
 
